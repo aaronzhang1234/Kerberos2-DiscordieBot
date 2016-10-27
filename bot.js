c2e83af4460721dfdb4511dfed0a9504f099fb2d
nimby <nimbyest@gmail.com>
2016-10-27 15:41:29 -0400

    votekicking now works, can votekick multiple peopel at once

--- bot.js
+++ bot.js
@@ -23,11 +23,9 @@ client.Dispatcher.on(Events.GATEWAY_READY, e=>
 );
 
 
-var a=0;
-var voted =[""];
 
-var stackofid=[];
-var prefix = "!f";
+
+var stackofid=[]; //2d array to be used for banning people;
 
 client.Dispatcher.on(Events.MESSAGE_CREATE,e=>{
     const content = e.message.content;
@@ -35,10 +33,20 @@ client.Dispatcher.on(Events.MESSAGE_CREATE,e=>{
     const guild = e.message.channel.guild;
     const author = e.message.author;
 
+    const personbannednogreat = content.replace(">","");
+    const idpersonbanned = personbannednogreat.substr(content.length-19);
+
+    var len = stackofid.length;
+
+
+
     //pings and testing
     if(content.indexOf("ping")==0) {
+        const personbannednogreat = content.replace(">","");
+        const idpersonbanned = personbannednogreat.substr(content.length-19);
+
         channel.sendMessage("pong");
-        console.log(prefix);
+
     }
 
     //help command
@@ -66,7 +74,9 @@ client.Dispatcher.on(Events.MESSAGE_CREATE,e=>{
         channel.sendMessage(eightball[Math.floor(Math.random()*20)]);
     }
 
+
     //joining voice channels
+    /*
     if(content.indexOf("!vjoin")==0){
 
         const targetChannel = content.replace("!vjoin ","");
@@ -76,86 +86,31 @@ client.Dispatcher.on(Events.MESSAGE_CREATE,e=>{
         if(vchannel)
             vchannel.join();
     }
+    */
 
 
+    //leaving all voice channels that the bot is in
+    /*
     if(content=="!vleave"){
         client.Channels
             .filter(channel=>channel.type== "voice" && channel.joined)
             .forEach(channel=>channel.leave());
     }
+    */
 
 
     //command to votekick someone
     if(content.indexOf("!startvotekick")==0){
-        var p = guild._members;
-        const personbanned = content.replace("!votekick @","");
-        const personbannednogreat = personbanned.replace(">","");
-        const idpersonbanned = personbannednogreat.substr(content.length-19);
 
-        var personnum=0;
-        console.log(idpersonbanned);
-
-        for(var p=0;p<guild.member_count;p++) {
-            if(idpersonbanned == guild.members[p].id){
-                console.log("hello")
-                personnum=p;
-            }
-            console.log(guild.members[p].id);
-        }
-
-
-        if(personnum!=0){
-            channel.sendMessage("<@"+author.id+"> has started a vote to kick <@"+guild.members[personnum].id+">");
-            setTimeout(function hello(){
-                channel.sendMessage("VOTING IS OVER");
-                channel.sendMessage(a+" people voted!");
-                if(a==Math.ceil(guild.member_count/2)){
-                    channel.sendMessage("goodbye <@"+guild.members[personnum].id+">");
-                    guild.members[personnum].kick();
-                }
-                else{
-                    channel.sendMessage("The votekick failed");
-                }
-
-                a=0;
-                voted =[];
-            },100000);
-
-        }
-       else{
-            channel.sendMessage("please choose someone else to kick");
-        }
-
-    }
-
-    //voting to kicksomeone.
-    if(content.indexOf("votekick")==0){
-        var didVote =false;
-        console.log(author.discriminator);
-        for(var o =0;o<voted.length;o++){
-            if(voted[o]==author.discriminator){
-                console.log(voted);
-                didVote = true;
-                channel.sendMessage("<@"+author.id+"> you already voted")
-            }
-        }
-        if(!didVote){
-            a++
-            voted.push(author.discriminator);
-            channel.sendMessage("<@"+author.id+"> has voted");
-        }
-    }
-
-    if(content.indexOf("stack")==0){
         var instack = false;
-        const personbanned = content.replace("stack @","");
-        const personbannednogreat = personbanned.replace(">","");
-        const idpersonbanned = personbannednogreat.substr(content.length-19);
 
         var personnum=0;
 
-        for(var i=0,len =stackofid.length;i!==len;i++){
+        for(var i=0;i!==len;i++){
             if(stackofid[i][0]==idpersonbanned){
+
+                /*The person who is getting votekicked is already in the stack getting judged
+                 */
                 instack = true;
                 channel.sendMessage("There is already a vote going to kick this person");
             }
@@ -163,53 +118,86 @@ client.Dispatcher.on(Events.MESSAGE_CREATE,e=>{
         if(!instack){
             for(var p=0;p<guild.member_count;p++) {
                 if(idpersonbanned == guild.members[p].id){
+                    //addding the person into the stack to get judged to get vote kicked
+
                     personnum=p;
                     stackofid.push([idpersonbanned,p,0]);
                     channel.sendMessage(idpersonbanned+ " has been stacked");
                 }
             }
             setTimeout(function hello(){
+                const votekicks = stackofid[0][2];
+                const num = stackofid[0][1];
+                const id = stackofid[0][0];
 
-                channel.sendMessage(stackofid[0][2] +" people voted to kick <@"+stackofid[0][0]+">");
+                channel.sendMessage(votekicks +" people voted to kick <@"+id+">");
+
+
+                //if more than half the people in the chat vote yes, the person gets kicked
+                if(votekicks >= Math.ceil(guild.member_count/2)){
+
+                    channel.sendMessage("<@"+id+"> has been kicked");
+
+                    guild.members[num].kick();
+                }
+                else{
+                    channel.sendMessage("Not enough votes have been gathered to kick <@"+id+">");
+                }
                 stackofid.shift();
             },30000)
         }
+
     }
-    if(content.indexOf("kick")==0){
-        var didvote=false;
+
+
+
+    /*voting to kick someone.
+      the command would be !votekick @<username>
+     */
+
+    if(content.indexOf("!votekick")==0){
+        var didVote=false;
         var isin=false;
-        const personbanned = content.replace("stack @","");
-        const personbannednogreat = personbanned.replace(">","");
-        const idpersonbanned = personbannednogreat.substr(content.length-19);
         var loc=0;
 
-        for(var i=0,len = stackofid.length;i!==len;i++){
+
+        //seraching for if the person that you want to kick is in the stack to kick them
+        for(var i=0;i!==len;i++){
             if(stackofid[i][0]==idpersonbanned){
                 isin=true;
                 loc =i;
             }
         }
         if(isin){
-                for(var o =3,len2=stackofid[loc].length;o!==len2;o++){
-                    if(stackofid[loc][o]==author.discriminator){
-                        console.log(voted);
-                        didVote = true;
-                        channel.sendMessage("<@"+author.id+"> you already voted")
-                    }
+            for(var o =3,len2=stackofid[loc].length;o!==len2;o++){
+                if(stackofid[loc][o]==author.discriminator){
+
+                    //searching through the stack to see if the person who is voting has already voted
+                    //if they find the person, they cannot vote twice
+                    didVote = true;
+                    channel.sendMessage("<@"+author.id+"> you already voted")
                 }
-                if(!didVote){
-                    a++
-                    stackofid[loc].push(author.discriminator);
-                    console.log(stackofid[loc]);
-                    stackofid[loc][2]++;
-                    channel.sendMessage("<@"+author.id+"> has voted");
-                }
+            }
+            if(!didVote){
+
+                //if the person did not vote, add their id to the stack of people voting to kick the person
+                //add one to the number of vote kicks
+                //say to the plebs that they have voted
+
+                stackofid[loc].push(author.discriminator);
+                stackofid[loc][2]++;
+                channel.sendMessage("<@"+author.id+"> has voted");
+            }
         }
         else{
+
+            //if the person has not had a vote on them, say it.
             channel.sendMessage("That person isn't in the stack right now, how about you stack them?");
         }
-
     }
+
+
+
 
     console.log(content);
 });
